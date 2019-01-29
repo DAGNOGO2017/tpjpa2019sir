2e2821d392658f56402d00c551feebe31b34d4b7
Dagnogo Kiko <kiko.dagnogo@etudiant.univ-rennes1.fr>
2019-01-29 13:05:06 +0100

    adding servlet

--- src/main/java/jpa/JpaTest.java
+++ src/main/java/jpa/JpaTest.java
@@ -1,33 +1,49 @@
 package jpa;
 
 import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
 import javax.persistence.PersistenceContext;
+
+import domain.User;
 
 public class JpaTest {
 
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
+	private EntityManager manager;
+	
+	public JpaTest(EntityManager manager) {
+        this.manager = manager;
+    }
 
-		EntityManager manager = EntityManagerHelper.getEntityManager();
-		EntityTransaction tx = manager.getTransaction();
-		tx.begin();
+	
+    public static void main(String[] args) {
+        EntityManagerFactory factory =   Persistence.createEntityManagerFactory("example");
+        EntityManager manager = factory.createEntityManager();
+        JpaTest test = new JpaTest(manager);
 
+        EntityTransaction tx = manager.getTransaction();
+        tx.begin();
+        try {
+            test.createUsers();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        tx.commit();
 
-		try {
+       // test.listUsers();
+            
+        manager.close();
+        System.out.println(".. done");
+    }
 
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		tx.commit();
-
-
-		manager.close();
-		EntityManagerHelper.closeEntityManagerFactory();
-		//		factory.close();
-	}
+    private void createUsers() {
+        int numOfUsers = manager.createQuery("Select u From User u", User.class).getResultList().size();
+        manager.persist(new User("Jakab Gipsz"));
+    }
 
 
 }

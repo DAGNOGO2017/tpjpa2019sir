8eca74ec8689d5200351a4812c7efe715f2a4851
Dagnogo Kiko <kiko.dagnogo@etudiant.univ-rennes1.fr>
2019-01-29 15:39:30 +0100

    recuperation via url ok

--- src/main/java/servlet/userServlet.java
+++ src/main/java/servlet/userServlet.java
@@ -3,40 +3,44 @@ package servlet;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(name="userServlet",
-urlPatterns={"/userServlet"})
+import domain.User;
+
+@WebServlet(name="userServlet", urlPatterns={"/userServlet"})
 public class userServlet extends HttpServlet{
-//	 @Override
-//	    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-//	            throws ServletException, IOException {
-//	            
-//	        PrintWriter p = new PrintWriter(resp.getOutputStream());
-//	        p.print("Hello world SIR");
-//	        p.flush();
-//	        
-//	    }
 
 	public void doPost(HttpServletRequest request, HttpServletResponse response)  
 			throws ServletException, IOException {
 	    response.setContentType("text/html");
 	    
-	    PrintWriter out = response.getWriter();  
-	    out.println("<HTML>\n<BODY>\n" +
-	                "<H1>Recapitulatif des informations</H1>\n" +
-	                "<UL>\n" +            
-	        " <LI>Nom: "
-	                + request.getParameter("nom") + "\n" +
-	                " <LI>Prenom: "
-	                + request.getParameter("prenom") + "\n" +
-	                " <LI>Age: "
-	                + request.getParameter("email") + "\n" +
-	                "</UL>\n" +                
-	        "</BODY></HTML>");
-		}
+	    PrintWriter out = response.getWriter(); 
+	    
+	    User user = new User();
+	    user.setNom(request.getParameter("nom"));
+	    user.setPrenom(request.getParameter("prenom"));
+	    user.setEmail(request.getParameter("email"));
+	    
+	    userServlet test = new userServlet();
+	    EntityManagerFactory factory =   Persistence.createEntityManagerFactory("dev");
+        EntityManager manager = factory.createEntityManager();
+        EntityTransaction tx = manager.getTransaction();
+        tx.begin();
+        try {
+            manager.persist(user);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        tx.commit();
+        manager.close();
+        System.out.println(".. done");
+	    }
 }
